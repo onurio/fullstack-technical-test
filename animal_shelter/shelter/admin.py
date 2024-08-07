8298464d4f95fc4d29fe4fe3f1c80930657b2983
Omri Nuri <omrinuri@gmail.com>
2024-08-07 11:58:50 -0500

    feat: registered models in the admin

--- animal_shelter/shelter/admin.py
+++ animal_shelter/shelter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Animal, Volunteer, Adoptant, Adoption
+
+admin.site.register(Animal)
+admin.site.register(Volunteer)
+admin.site.register(Adoptant)
+admin.site.register(Adoption)
